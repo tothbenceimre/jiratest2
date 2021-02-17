7a19b3249fbb07d4a1a069b50832af2faaaae84c
Zsofia Szego <szegozsofia@gmail.com>
2021-02-17 13:36:51 +0100

    add ComponentPage

---
+++ jiratest2/src/main/java/pages/ComponentPage.java
@@ -0,0 +1,70 @@
+package pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ComponentPage {
+    WebDriver driver;
+    @FindBy(xpath = "//form[@id='components-add__component']//input[@name= 'name']") WebElement componentName;
+    @FindBy(xpath = "//*[@id='leadUserName-single-select']/input") WebElement lead;
+    @FindBy(xpath = "//input[@name='description']") WebElement description;
+    @FindBy(xpath = "//*[@id='assigneeType-single-select']/input") WebElement assignee;
+    @FindBy(xpath = "//form[@id='components-add__component']//button") WebElement addButton;
+    @FindBy(xpath = "//*[@id='content-container']") WebElement container;
+    @FindBy(xpath = "//*[@id='components-table']/tbody[@class='items']/tr[1]") WebElement tr;
+
+    public ComponentPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    public void fill(WebElement element, String msg){
+        element.sendKeys(Keys.CONTROL+"a");
+        element.sendKeys(Keys.DELETE);
+        element.sendKeys(msg);
+        element.sendKeys(Keys.TAB);
+    }
+
+    public void fillName(String name){
+        fill(componentName, name);
+    }
+
+    public void fillLead(String leadName){
+        fill(lead, leadName);
+    }
+
+    public void fillDescription(String desc){
+        fill(description, desc);
+    }
+
+    public void fillAssignee(String asg){
+        fill(assignee, asg);
+    }
+
+    public void clickAddButton(){
+        addButton.click();
+    }
+
+    public String getId(){
+//        waitForStale(tr);
+        return tr.getAttribute("data-component-id");
+    }
+
+    public void refresh(){
+        driver.navigate().refresh();
+    }
+
+    public void waitForStale(WebElement element){
+        try {
+            element.click();
+        } catch (StaleElementReferenceException e){
+            new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(element));
+        }
+    }
+}
